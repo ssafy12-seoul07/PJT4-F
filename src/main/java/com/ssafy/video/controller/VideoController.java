bafa803447fd803e89951890bcd792b0babc3e0f
Huk9uri <huk9uri@gmail.com>
2024-09-27 15:34:10 +0900

    video 상세 정보와 해당 video에 대한 리뷰를 상세페이지에 보냅니다

--- src/main/java/com/ssafy/video/controller/VideoController.java
+++ src/main/java/com/ssafy/video/controller/VideoController.java
@@ -3,6 +3,9 @@ package com.ssafy.video.controller;
 import java.io.IOException;
 import java.util.List;
 
+import com.ssafy.review.model.dto.Review;
+import com.ssafy.review.model.service.ReviewService;
+import com.ssafy.review.model.service.ReviewServiceImpl;
 import com.ssafy.video.model.dto.Video;
 import com.ssafy.video.model.service.VideoService;
 import com.ssafy.video.model.service.VideoServiceImpl;
@@ -17,37 +20,44 @@ import jakarta.servlet.http.HttpServletResponse;
 public class VideoController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
-	private VideoService service = VideoServiceImpl.getInstance();
-	
+
+	private VideoService videoService = VideoServiceImpl.getInstance();
+	private ReviewService reviewService = ReviewServiceImpl.getInstance();
+
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String action = request.getParameter("action");
-		
+
 		switch (action) {
 		case "getList":
-			doGetList(request,response);
+			doGetList(request, response);
 			break;
 		case "videoDetail":
-			doVideoDetail(request,response);
+			doVideoDetail(request, response);
 			break;
 		default:
 			break;
 		}
-		
-	
+
 	}
 
-	private void doVideoDetail(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		
+	private void doVideoDetail(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Integer videoId = Integer.parseInt(request.getParameter("videoId"));
+		List<Review> reviewList = reviewService.getReviewListOfVideo(videoId);
+		Video video = videoService.getVideo(videoId);
+		request.setAttribute("reviewList", reviewList);
+		request.setAttribute("video", video);
+		request.getRequestDispatcher("/WEB-INF/video/detail.jsp").forward(request, response);
+
 	}
 
-	private void doGetList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Video>	videoList = service.getList();
+	private void doGetList(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<Video> videoList = videoService.getList();
 		request.setAttribute("videoList", videoList);
 		request.getRequestDispatcher("/WEB-INF/video/videolist.jsp").forward(request, response);
-		
 	}
 
 }
