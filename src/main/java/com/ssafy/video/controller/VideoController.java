3b69d65e6de59864dc48033c539bbae9d36c5154
Huk9uri <huk9uri@gmail.com>
2024-09-27 12:24:46 +0900

    add package

---
+++ src/main/java/com/ssafy/video/controller/VideoController.java
@@ -0,0 +1,13 @@
+package com.ssafy.video.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/video")
+public class VideoController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
